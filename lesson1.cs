c467971b4d1863a9ed09d47f19ac939f6215af80
za4emmne <iluhaha@mail.ru>
2022-05-20 12:27:21 +0500

    Create lesson1.cs

---
+++ lesson1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ijunior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isDead = false;
+            string weapon = "gun";
+            int level = 99;
+            byte page = 3;
+            short length = 23;
+            char coma = '.';
+            sbyte temperature = -13;
+            long number = 8800553535;
+            float weight = 85.3f;
+            double lenght = 33.6;
+        }
+    }
+}
