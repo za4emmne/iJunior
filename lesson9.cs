a4d61d3c197369936ffc2d953bb5070b90743649
za4emmne <iluhaha@mail.ru>
2022-05-21 21:32:09 +0500

    Update lesson9.cs

--- lesson9.cs
+++ lesson9.cs
@@ -7,17 +7,18 @@ namespace lesson
         static void Main(string[] args)
         {
             string message;
+            string exit = "exit";
             bool isExit = false;
             int itteration = 1;
             Console.WriteLine("Запустился счетчик иттераций");
 
             while (isExit == false)
-            {
-                Console.Write("Нажмините любую клавишу чтобы продолжить, либо введите exit, чтобы выйти: ");
-               message = Console.ReadLine();
+            { 
+                Console.Write("Нажмините любую клавишу чтобы продолжить, либо введите "+ exit +", чтобы выйти: ");
+                message = Console.ReadLine();
                 Console.WriteLine(itteration++);
 
-                if (message == "exit")
+                if (message == exit)
                 {
                     isExit = true;
                 }
