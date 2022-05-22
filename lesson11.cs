dcf399dfc4965caf755a9de5c61a187d6415aa99
za4emmne <iluhaha@mail.ru>
2022-05-22 12:20:43 +0500

    Update lesson11.cs

--- lesson11.cs
+++ lesson11.cs
@@ -6,24 +6,34 @@ namespace lesson
     {
         static void Main(string[] args)
         {
+            int firstNumber = 0;
+            int lastNumber = 101;
             Random random = new Random();
-            int number = random.Next(0, 100);
+            int number = random.Next(firstNumber, lastNumber);
             int sum = 0;
+            int numberOne = 3;
+            int numberTwo = 5;
             Console.WriteLine("Случайно число: " + number);
 
-            for (int i = 0; i < number; i += 3)
+            for (int i = 0; i <= number; i += numberOne)
             {
                 sum += i;
                 Console.WriteLine(i);
             }
 
-            for (int n = 0; n < number; n += 5)
+            for (int n = 0; n <= number; n += numberTwo)
             {
+
+                if (n % numberOne == 0)
+                {
+                    continue;
+                }
                 sum += n;
                 Console.WriteLine(n);
             }
-            Console.WriteLine("Сумма всех чисел: " + (sum + number));
+            Console.WriteLine("Сумма всех чисел: " + sum);
 
         }
     }
 }
+
