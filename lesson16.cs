eab072e073ac33f40e3435e86706e27f43528bbb
za4emmne <iluhaha@mail.ru>
2022-05-24 00:22:12 +0500

    Update lesson16.cs

--- lesson16.cs
+++ lesson16.cs
@@ -6,18 +6,20 @@ namespace lesson
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            int number = rand.Next(0, 1000000);
-            int sqr = 1;
-            int numberTwo = 2;
-            int sqrTwo = 2;
+            int firstNumber = 0;
+            int lastNumber = 1000000;
+            Random random = new Random();
+            int number = random.Next(firstNumber, lastNumber);
+            int sqruare = 1;
+            int findNumber = 2;
+            int squareFindNumber = 2;
 
-            for (int i = 1; number>sqrTwo; i++)
+            for (int i = 1; number > squareFindNumber; i++)
             {
-                sqrTwo = sqrTwo * numberTwo;
-                sqr = i;
+                squareFindNumber = squareFindNumber * findNumber;
+                sqruare = i;
             }
-            Console.WriteLine("Число из рандома: " + number + "\nМинимальная степень " + sqr + ", а число в этой степени - " + sqrTwo);
+            Console.WriteLine("Число из рандома: " + number + "\nМинимальная степень " + sqruare + ", а число в этой степени - " + squareFindNumber);
         }
     }
 }
