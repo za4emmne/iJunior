c52df971b63d7d15244ffa7d76e73fd0eefd43ae
za4emmne <iluhaha@mail.ru>
2022-05-25 00:03:02 +0500

    Create lesson21.cs

---
+++ lesson21.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeMatrix = 5;
+            int[,] matrix = new int[sizeMatrix, sizeMatrix];
+            Random random = new Random();
+            int minRandom = 1;
+            int maxRandom = 4;
+            int sumRow = 0;
+            int multColom = 1;
+            int numberRow = 1;
+            int numberColom = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(minRandom, maxRandom);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                sumRow += matrix[numberRow, j];
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                multColom *= matrix[i, numberColom];
+            }
+            Console.WriteLine("\nСумма элементов второй строки: " + sumRow);
+            Console.WriteLine("\nПроизведение первого столбца: " + multColom);
+        }
+    }
+}
