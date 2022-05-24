c70d25f46d7a7eaa72d291cb84cfed48c828089a
za4emmne <iluhaha@mail.ru>
2022-05-25 00:08:15 +0500

    Update lesson20.cs

--- lesson20.cs
+++ lesson20.cs
@@ -6,41 +6,41 @@ namespace lesson
     {
         static void Main(string[] args)
         {
-            int[,] A = new int[10, 10];
+            int[,] matrix = new int[10, 10];
             Random random = new Random();
             int maxValue = 0;
             int minValue = 0;
             int topMatrix = 100;
             int downMatrix = 0;
 
-            for (int i = 0; i < A.GetLength(0); i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
 
-                for (int j = 0; j < A.GetLength(1); j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    A[i, j] = random.Next(downMatrix, topMatrix);
-                    Console.Write(A[i, j] + "  ");
+                    matrix[i, j] = random.Next(downMatrix, topMatrix);
+                    Console.Write(matrix[i, j] + "  ");
 
-                    if (A[i, j] > maxValue)
+                    if (matrix[i, j] > maxValue)
                     {
-                        maxValue = A[i, j];
+                        maxValue = matrix[i, j];
                     }
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("\nМаксимальный элемент матрицы: " + maxValue + "\n");
 
-            for (int i = 0; i < A.GetLength(0); i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
 
-                for (int j = 0; j < A.GetLength(1); j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
 
-                    if (A[i, j] == maxValue)
+                    if (matrix[i, j] == maxValue)
                     {
-                        A[i, j] = minValue;
+                        matrix[i, j] = minValue;
                     }
-                    Console.Write(A[i, j] + "  ");
+                    Console.Write(matrix[i, j] + "  ");
                 }
                 Console.WriteLine();
             }
