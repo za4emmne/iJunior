82f92704f0b72edd5f92bddeb00d6e75af180053
za4emmne <iluhaha@mail.ru>
2022-05-26 00:07:16 +0500

    Create lesson23.cs

---
+++ lesson23.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isExit = false;
+            bool isSum = false;
+            string userInput = "";
+            int number;
+            int sum = 0;
+            int[] array = new int[1];
+
+            while (isExit == false)
+            {
+                Console.Write("Введите команду:\n sum - суммировать все введенные числа\n exit - выход\nИли введите число: ");
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "exit":
+                        {
+                            isExit = true;
+                            break;
+                        }
+                    case "sum":
+                        {
+                            for (int i = 0; i < array.Length; i++)
+                            {
+                                sum += array[i];
+                            }
+                            isSum = true;
+                            break;
+                        }
+                    default:
+                        {
+                            number = Convert.ToInt32(userInput);
+                            array[array.Length - 1] = number;
+                            int[] tempArray = new int[array.Length + 1];
+
+                            for (int i = 0; i < array.Length; i++)
+                            {
+                                Console.WriteLine(array[i]);
+                            }
+
+                            for (int i = 0; i < array.Length; i++)
+                            {
+                                tempArray[i] = array[i];
+                            }
+                            array = tempArray;
+                            break;
+                        }
+                }
+                Console.Clear();
+
+                if (isSum == true)
+                {
+                    Console.WriteLine("Cумма элементов массива = " + sum + "\n");
+                }
+            }
+        }
+    }
+}
