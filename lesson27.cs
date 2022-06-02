9ad1a442d0b7b4acd4852bd3625ed546135e2383
za4emmne <iluhaha@mail.ru>
2022-06-02 22:35:54 +0500

    Update lesson27.cs

--- lesson27.cs
+++ lesson27.cs
@@ -8,18 +8,17 @@ namespace lesson
         {
             int userInput;
             int tempArrayNumber;
-            int tempArrayShift;
             Random random = new Random();
             int minRandom = 5;
             int maxRandom = 10;
-            int arrayValue = random.Next(minRandom,maxRandom);
+            int arrayValue = random.Next(minRandom, maxRandom);
             int[] array = new int[arrayValue];
 
             Console.WriteLine("Исходный массив: ");
 
             for (int i = 0; i < arrayValue; i++)
             {
-                array[i] = i+1;
+                array[i] = i + 1;
                 Console.Write(array[i] + " ");
             }
             Console.WriteLine("\nВведите на сколько сдвнуть массив");
@@ -28,14 +27,12 @@ namespace lesson
             for (int i = 0; i < userInput; i++)
             {
                 tempArrayNumber = array[0];
-                array[0] = array[arrayValue - 1];
 
-                for (int j = 1; j < arrayValue; j++)
+                for (int j = 0; j < arrayValue-1; j++)
                 {
-                    tempArrayShift = array[j];
-                    array[j] = tempArrayNumber;
-                    tempArrayNumber = tempArrayShift;
+                    array[j] = array[j + 1];
                 }
+                array[arrayValue-1] = tempArrayNumber;
             }
             Console.WriteLine("Новый массив: ");
 
