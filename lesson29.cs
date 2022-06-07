6befe3ed21fabbfefe219e2d09704981e95b034d
za4emmne <iluhaha@mail.ru>
2022-06-07 23:58:42 +0500

    Update lesson29.cs

--- lesson29.cs
+++ lesson29.cs
@@ -11,8 +11,6 @@ namespace lesson
             string[] name = new string[] { "Аркаев Аркадий Артемович", "Беломоров Борис Бентарович" };
             string[] job = new string[] { "Писатель", "Лучник" };
 
-
-
             while (isExit == false)
             {
                 Console.WriteLine("\n1. Добавить досье\n2. Вывести все досье\n3. Удалить досье\n4. Поиск по фамилии\n5. Выход");
@@ -23,10 +21,7 @@ namespace lesson
                 {
                     case "1":
                         {
-                            Console.Write("Введите ФИО: ");
-                            name = AddArray(name);
-                            Console.Write("Введите должность: ");
-                            job = AddArray(job);
+                            name = AddArray(name, "ФИО"); job = AddArray(job, "должность");
                             break;
                         }
                     case "2":
@@ -36,17 +31,12 @@ namespace lesson
                         }
                     case "3":
                         {
-                            Console.Write("\nВведите номер досье, которое хотите удалить: ");
-                            int delElement = Convert.ToInt32(Console.ReadLine());
-                            DelArray(ref name, delElement);
-                            DelArray(ref job, delElement);
+                            deleteCase(ref name, ref job);
                             break;
                         }
                     case "4":
                         {
-                            Console.Write("\nВведите фамилию: ");
-                            string userInputName = Console.ReadLine();
-                            FindName(name, job, userInputName);
+                            FindName(name, job);
                             break;
                         }
                     case "5":
@@ -58,8 +48,17 @@ namespace lesson
             }
         }
 
-        static string[] AddArray(string[] array)
+        static void deleteCase(ref string[] name, ref string[] job)
         {
+            Console.Write("\nВведите номер досье, которое хотите удалить: ");
+            int deleteElement = Convert.ToInt32(Console.ReadLine());
+            DeleteArray(ref name, deleteElement);
+            DeleteArray(ref job, deleteElement);
+        }
+
+        static string[] AddArray(string[] array, string input)
+        {
+            Console.Write($"Введите {input}: ");
             string userInput = Console.ReadLine();
             string[] tempName = new string[array.Length + 1];
 
@@ -72,16 +71,18 @@ namespace lesson
             return array;
         }
 
-        static string[] DelArray(ref string[] array, int numberElement)
+        static void DeleteArray(ref string[] array, int numberElement)
         {
             string[] tempArray = new string[array.Length - 1];
 
             if (numberElement <= array.Length)
             {
+
                 for (int i = 0; i < numberElement - 1; i++)
                 {
                     tempArray[i] = array[i];
                 }
+
                 for (int i = numberElement; i < array.Length; i++)
                 {
                     tempArray[i - 1] = array[i];
@@ -92,31 +93,32 @@ namespace lesson
             {
                 Console.WriteLine("Такого досье нет.");
             }
-            return array;
         }
 
         static void DrawArray(string[] arrayName, string[] arrayJob)
         {
+
             for (int i = 0; i < arrayName.Length; i++)
             {
                 Console.WriteLine("\n" + (i + 1) + ". " + arrayName[i] + " - " + arrayJob[i]);
             }
         }
 
-        static void FindName(string[] nameArray, string[] jobArray, string surname)
+        static void FindName(string[] nameArray, string[] jobArray)
         {
+            Console.Write("\nВведите фамилию: ");
+            string userInputName = Console.ReadLine();
 
             for (int i = 0; i < nameArray.Length; i++)
             {
-                if (nameArray[i].Contains(surname) == true)
+
+                if (nameArray[i].Contains(userInputName) == true)
                 {
                     Console.WriteLine($"\n{i + 1}. {nameArray[i]} - {jobArray[i]}");
-                    break;
                 }
                 else
                 {
-                    Console.WriteLine("Такой фамилии нет.");
-                    break;
+                    continue;
                 }
             }
         }
