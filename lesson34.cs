b85333f6825d09bbf0993c082ab11800516b4bc1
za4emmne <iluhaha@mail.ru>
2022-06-27 22:44:48 +0500

    Create lesson34.cs

---
+++ lesson34.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isExit = false;
+            Dictionary<string, string> wordBook = new Dictionary<string, string>();
+            string inputWord = "";
+            wordBook.Add("Машина", "Средство передвижения на колесах с двигателем");
+
+            while (isExit == false)
+            {
+                Console.WriteLine("Выберите действие: \n1. Поиск из словаря\n2. Добавить слово в словарь\n3. Выход");
+                int chooseMenu = Convert.ToInt32(Console.ReadLine());
+                switch (chooseMenu)
+                {
+                    case 1:
+                        FindWord(inputWord, wordBook);
+                        break;
+                    case 2:
+                        AddWord(ref wordBook);
+                        break;
+                    case 3:
+                        isExit = true;
+                        break;
+                }
+            }
+        }
+
+        static void AddWord(ref Dictionary<string, string> wordBook)
+        {
+            Console.Write("Ввндите слово: ");
+            string newWord = Console.ReadLine();
+            Console.Write("Введите значение слова: ");
+            string wordValue = Console.ReadLine();
+            wordBook.Add(newWord, wordValue);
+            Console.Write("Слово добавлено, нажмите любую клавишу");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void FindWord(string input, Dictionary<string, string> wordBook)
+        {
+            Console.Write("Чтобы получить значение, введите слово из словаря: ");
+            input = Console.ReadLine();
+            
+            if (wordBook.ContainsKey(input))
+            {
+                foreach (var word in wordBook)
+                {
+
+                    if (input == word.Key)
+                    {
+                        Console.WriteLine($"{word.Key} - {word.Value}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Такого слова нет.");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
