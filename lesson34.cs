e2cabdc337950acb491fa3fb213ec4e43d70dc8c
za4emmne <iluhaha@mail.ru>
2022-07-12 23:42:12 +0500

    Update lesson34.cs

--- lesson34.cs
+++ lesson34.cs
@@ -12,30 +12,29 @@ namespace homework
         {
             bool isExit = false;
             Dictionary<string, string> wordBook = new Dictionary<string, string>();
-            string inputWord = "";
             wordBook.Add("Машина", "Средство передвижения на колесах с двигателем");
 
             while (isExit == false)
             {
                 Console.WriteLine("Выберите действие: \n1. Поиск из словаря\n2. Добавить слово в словарь\n3. Выход");
-                int chooseMenu = Convert.ToInt32(Console.ReadLine());
+                string chooseMenu = Console.ReadLine();
 
                 switch (chooseMenu)
                 {
-                    case 1:
-                        FindWord(inputWord, wordBook);
+                    case "1":
+                        FindWord(wordBook);
                         break;
-                    case 2:
-                        AddWord(ref wordBook);
+                    case "2":
+                        AddWord(wordBook);
                         break;
-                    case 3:
+                    case "3":
                         isExit = true;
                         break;
                 }
             }
         }
 
-        static void AddWord(ref Dictionary<string, string> wordBook)
+        static void AddWord(Dictionary<string, string> wordBook)
         {
             Console.Write("Введите слово: ");
             string newWord = Console.ReadLine();
@@ -53,10 +52,10 @@ namespace homework
             Console.Clear();
         }
 
-        static void FindWord(string input, Dictionary<string, string> wordBook)
+        static void FindWord(Dictionary<string, string> wordBook)
         {
             Console.Write("Чтобы получить значение, введите слово из словаря: ");
-            input = Console.ReadLine();
+            string input = Console.ReadLine();
 
             if (wordBook.ContainsKey(input))
             {
