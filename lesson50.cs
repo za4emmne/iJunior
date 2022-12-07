c2d31d0ce2e0a92ed2198bcb6ce074afdc6facac
za4emmne <iluhaha@mail.ru>
2022-12-07 20:27:46 +0500

    Update lesson50.cs

--- lesson50.cs
+++ lesson50.cs
@@ -13,35 +13,33 @@ namespace lesson
             bool isExit = false;
             int maxCountFish = 15;
             int oldSpeed = 1;
-            const string commandAddFish = "1";
-            const string commandRemoveFish = "2";
-            const string commandExit = "q";
+            const string CommandAddFish = "1";
+            const string CommandRemoveFish = "2";
+            const string CommandExit = "q";
             Aquarium aquarium = new Aquarium(maxCountFish);
 
             while (isExit == false)
             {
                 aquarium.ShowStats(oldSpeed);
-                Console.WriteLine($"\nВы можете:\nДобавить рыбку в аквариум - НАЖАВ {commandAddFish}\nДостать рыбку - НАЖАВ {commandRemoveFish}\nВыйти - НАЖАВ {commandExit}");
+                Console.WriteLine($"\nВы можете:\nДобавить рыбку в аквариум - НАЖАВ {CommandAddFish}\nДостать рыбку - НАЖАВ {CommandRemoveFish}\nВыйти - НАЖАВ {CommandExit}");
                 Console.Write("ваш выбор: ");
                 string playerChoose = Console.ReadLine();
 
                 switch (playerChoose)
                 {
-                    case commandAddFish:
+                    case CommandAddFish:
                         aquarium.AddFish();
                         break;
-                    case commandRemoveFish:
+                    case CommandRemoveFish:
                         aquarium.RemoveFish();
                         break;
-                    case commandExit:
+                    case CommandExit:
                         isExit = true;
-                        break;
-                    default:
                         break;
                 }
 
-                aquarium.GetFishsOld(oldSpeed);
-                aquarium.FishDead();
+                aquarium.GetOldFish(oldSpeed);
+                aquarium.DieFish();
                 Console.Clear();
             }
         }
@@ -50,41 +48,50 @@ namespace lesson
     class Aquarium
     {
         private Random _random = new Random();
-        private List<Fish> _pisces = new List<Fish>();
-        private int MaxCountFish;
+        private List<Fish> _fishes = new List<Fish>();
+        private int _maxCountFish;
         public Aquarium(int maxCountFish)
         {
-            MaxCountFish = maxCountFish;
+            _maxCountFish = maxCountFish;
             CreateFishs();
         }
 
-        public void FishDead()
+        public void DieFish()
         {
-            for (int i = 0; i<_pisces.Count; i++)
+            for (int i = 0; i < _fishes.Count; i++)
             {
-                if (_pisces.ElementAt(i).Life < 0)
+                if (_fishes[i].Life < 0)
                 {
-                    _pisces.RemoveAt(i);
+                    _fishes.RemoveAt(i);
+                    i--;
                 }
             }
         }
 
         public void RemoveFish()
         {
-            if (_pisces.Count > 0)
+            if (_fishes.Count > 0)
             {
                 Console.Write("Введите номер той рыбки, которую хотите достать: ");
                 string playerChoose = Console.ReadLine();
                 int.TryParse(playerChoose, out int fishNumber);
-                _pisces.RemoveAt(fishNumber - 1);
+
+                if (fishNumber > 0 && fishNumber < _fishes.Count)
+                {
+                    _fishes.RemoveAt(fishNumber - 1);
+                }
+                else
+                {
+                    Console.WriteLine("Рыбы с таким номер нет");
+                }
             }
         }
 
         public void AddFish()
         {
-            if (_pisces.Count < MaxCountFish)
+            if (_fishes.Count < _maxCountFish)
             {
-                _pisces.Add(CreateFish());
+                _fishes.Add(CreateFish());
             }
             else
             {
@@ -94,21 +101,22 @@ namespace lesson
 
         public void ShowStats(int oldSpeed)
         {
-            Console.WriteLine("Это аквариум, в нем сейчас живет - " + _pisces.Count + " рыб\nВ него помещается максимум " + MaxCountFish + " рыб\nПосле " +
+            Console.WriteLine("Это аквариум, в нем сейчас живет - " + _fishes.Count + " рыб\nВ него помещается максимум " + _maxCountFish + " рыб\nПосле " +
                 "каждого действия рыбка стареет на " + oldSpeed + " жизнь");
+            int numberFish = 1;
 
-            foreach (var fish in _pisces)
+            foreach (var fish in _fishes)
             {
-                int numberFish = _pisces.IndexOf(fish) + 1;
                 Console.Write("Рыба № " + numberFish + " - ");
                 fish.ShowLife();
                 Console.WriteLine();
+                numberFish++;
             }
         }
 
-        public void GetFishsOld(int oldSpeed)
+        public void GetOldFish(int oldSpeed)
         {
-            foreach (var fish in _pisces)
+            foreach (var fish in _fishes)
             {
                 fish.GetOld(oldSpeed);
             }
@@ -117,12 +125,12 @@ namespace lesson
         private void CreateFishs()
         {
             int minCountFish = 1;
-            int maxCountFish = MaxCountFish + 1;
+            int maxCountFish = _maxCountFish + 1;
             int countFishs = _random.Next(minCountFish, maxCountFish);
 
             for (int i = 0; i < countFishs; i++)
             {
-                _pisces.Add(CreateFish());
+                _fishes.Add(CreateFish());
             }
         }
 
@@ -158,7 +166,7 @@ namespace lesson
             {
                 Console.Write(" !Рыбка умрет на следующем ходу!");
             }
-            else if(Life == 0)
+            else if (Life == 0)
             {
                 Console.Write(" Рыбка умерла :(");
             }
